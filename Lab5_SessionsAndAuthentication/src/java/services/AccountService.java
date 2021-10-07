fa730099a0b555a39d41023ef702c3bbee2dba3c
rawbz <rmjbalanag@gmail.com>
2021-10-07 12:21:35 -0600

    Created jsp, servlets, services and models

---
+++ Lab5_SessionsAndAuthentication/src/java/services/AccountService.java
@@ -0,0 +1,19 @@
+
+package services;
+
+import models.User;
+
+/**
+ *
+ * @author rmjba
+ */
+public class AccountService {
+    public User login(String username, String password){
+        
+        if(username.equals("abe") || username.equals("barb") && password.equals("password")){
+            return new User(username, null);
+        }
+        return null;
+    }
+    
+}
